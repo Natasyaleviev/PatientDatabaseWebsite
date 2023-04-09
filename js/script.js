c86c0ddd187e2e4db99ad1d5dec7d5ed921bb0b9
Natasyaleviev <123341853+Natasyaleviev@users.noreply.github.com>
2023-04-08 20:17:44 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,21 +7,3 @@ function closePopup() {
     document.querySelector('.popup').style.display = 'none';
     document.querySelector('.overlay').style.display = 'none';
   }
-
-
-function GetSingleItem() {
-  if (!document.getElementsByClassName("selected").length) {
-    alert("Please select an item from the table");
-    return;
-  }
-  const id =
-    document.getElementsByClassName("selected")[0].childNodes[0].innerHTML;
-  document.getElementById("id").value = id;
-  const description =
-    document.getElementsByClassName("selected")[0].childNodes[4].innerHTML;
-  document.getElementById("description").value = description;
-  const status =
-    document.getElementsByClassName("selected")[0].childNodes[3].innerHTML;
-  document.getElementById("status").value = status;
-  $("#modform").show();
-}
